7f69b673ea937c5277efa9efd5c7cca76397c243
mesihg <37037773+mesihg@users.noreply.github.com>
2018-03-11 16:01:12 +0300

    Update app.js

--- mod2_solution/js/app.js
+++ mod2_solution/js/app.js
@@ -25,7 +25,7 @@
   function ShoppingListCheckOffService() {
     var service = this;
     var toBuyItems = [
-      { name: "cookies", quantity: 5 },
+      { name: "cookies", quantity: 7 },
       { name: "milk", quantity: 3 },
       { name: "coke", quantity: 4},
       { name: "Chocolate", quantity: 10 },
